6d9191a44f683738e265cfcb7ff197a8ed89854d
Rafael Soares <139012645+SuperZNnn@users.noreply.github.com>
2024-11-05 16:45:13 -0300

    Revert "002 homepage"

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import { BrowserRouter } from 'react-router-dom'
 import './App.css'
-import AppRoutes from './routes'
 
 function App() {
 
   return (
     <>
-      <BrowserRouter>
-        <AppRoutes/>
-      </BrowserRouter>
+      
     </>
   )
 }
