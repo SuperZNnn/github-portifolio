1ec7d1385a81b9d5a57adcf770596bc907419245
Rafael Soares <rafael.soares.pb@compasso.com.br>
2024-11-06 18:23:53 -0300

    feat: firebase agora retorna o screeName

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,10 +1,18 @@
+import firebase from "firebase/compat/app"
 import { auth, githubProvider } from "../config/firebase"
 import { signInWithPopup } from "firebase/auth"
+
+interface CustomUser extends firebase.User{
+    reloadUserInfo: {
+        screenName: string
+    }
+}
 
 export const signIn = () => {
     signInWithPopup(auth, githubProvider)
     .then((result) => {
-        console.log(result.user)
+        const user = result.user as CustomUser
+        console.log(user.reloadUserInfo.screenName)
     })
     .catch((error) => {
         console.log(error)
