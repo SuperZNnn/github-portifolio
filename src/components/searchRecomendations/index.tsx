59d008e59ebbb5a551af6fe26bddbdbaa75f2bcd
Rafael Soares <rafael.soares.pb@compasso.com.br>
2024-11-10 19:38:41 -0300

    feat: Adicionado sistema de pesquisa

--- src/components/searchRecomendations/index.tsx
+++ src/components/searchRecomendations/index.tsx
@@ -1,13 +1,31 @@
-import { SearchBox } from "./style"
+import { useEffect, useState } from "react";
+import { SearchBox } from "./style";
+import { getAllUsers, User } from "../../hooks/storeUsersData";
 
-const SearchRecomendations = () => {
+const SearchRecomendations = ({ search, complete }: { search?: string, complete: (name: string) => void }) => {
+    const users = getAllUsers();
+    const [foundedUsers, setFoundedUsers] = useState<Array<User>>([])
+
+    useEffect(() => {
+        if (search) {
+            setFoundedUsers(
+                users.filter((user: User) => user.name?.startsWith(search))
+            );
+        } else {
+            setFoundedUsers([])
+        }
+    }, [search])
+
     return (
         <SearchBox>
-            <div className="user">
-                <img src="/assets/images/default_user.png"/>
-                <p>rafinha</p>
-            </div>
+            {foundedUsers.map((user: User, index: number) => (
+                <div className="user" key={index} onClick={() => {complete(user.name)}}>
+                    <img src="/assets/images/default_user.png" alt="User" />
+                    <p>{user.name}</p>
+                </div>
+            ))}
         </SearchBox>
-    )
-}
-export default SearchRecomendations
+    );
+};
+
+export default SearchRecomendations;
