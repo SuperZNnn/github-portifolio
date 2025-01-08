37c7baadd0259f9acad1f30aa88c7974f5f2fcb7
Rafael Soares <rafael.soares.pb@compasso.com.br>
2025-01-08 11:27:04 -0300

    feat: Criado .env

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app"
 import { getAuth, GithubAuthProvider } from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAHvQxDyqnjRpDYT2I7I7HVUXcXlINEjlA",
-  authDomain: "git-port-c55c7.firebaseapp.com",
-  projectId: "git-port-c55c7",
-  storageBucket: "git-port-c55c7.firebasestorage.app",
-  messagingSenderId: "38354659538",
-  appId: "1:38354659538:web:6c614f88f39bd2185d250b",
-  measurementId: "G-NHXWF08Z2V"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 }
 
 const app = initializeApp(firebaseConfig)
